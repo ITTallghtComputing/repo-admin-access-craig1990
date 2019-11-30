f0d0e974af0c0450e37b3b59dd79dabbe9df194f
Craig <x00075734@mytudublin.ie>
2019-11-30 17:44:42 +0000

    change start page to booking page

--- BookingSystem/App_Start/RouteConfig.cs
+++ BookingSystem/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace BookingSystem
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "School", action = "Booking", id = UrlParameter.Optional }
             );
         }
     }
